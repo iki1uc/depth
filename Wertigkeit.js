0136675239261a19b99cab52d41ca594f100c56d
iki1uc <entertrainer@web.de>
2026-08-22 07:52:39 +0200

    Update Wertigkeit.js

--- Wertigkeit.js
+++ Wertigkeit.js
@@ -1,12 +1,10 @@
-// Wertigkeit.js
 export const Wertigkeit = {
-    scale: { ICE: 1, HOT: 2, HYBRID: 3, UNLOGIKAL: 5 },
-
-    evaluate(mode) {
-        return {
-            mode,
-            value: this.scale[mode] ?? 0,
-            note: "Wertigkeit berechnet"
-        };
+    scale: {
+        GPU: 3,   // Compute
+        CPU: 2,   // Control
+        RAM: 1    // Memory
+    },
+    eval(hw) {
+        return { hw, value: this.scale[hw] };
     }
 };
