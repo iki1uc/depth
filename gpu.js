ddd2f9d150a1ae294833b325f60d5f188ec1dda9
iki1uc <entertrainer@web.de>
2026-08-22 07:04:50 +0200

    Create gpu.js

---
+++ gpu.js
@@ -0,0 +1,18 @@
+export const GPU = {
+
+    rom(x) {
+        return x + "_rom"
+    },
+
+    fusion(x) {
+        return x + "_fusion"
+    },
+
+    render(x) {
+        return x + "_render"
+    },
+
+    vitality(x) {
+        return x + "_360v"
+    }
+}
