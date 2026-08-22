fbaed50410a15012ec161523f60d5664feb7f8b3
iki1uc <entertrainer@web.de>
2026-08-22 07:41:44 +0200

    Update UM.x.js

--- UM.x.js
+++ UM.x.js
@@ -1,18 +1,12 @@
-D3D_FEATURE_LEVEL levels[] = {
-    D3D_FEATURE_LEVEL_11_0,
-    D3D_FEATURE_LEVEL_10_0,
-    D3D_FEATURE_LEVEL_9_3
+// RUM.js – DX10 Layer (Bindungs-Schicht)
+export const RUM = {
+    featureLevel: "10_0",
+    axiom: ["tmpC", "tmpD"],
+    init() {
+        return {
+            dx: "DX10-RUM initialized",
+            axiom: this.axiom,
+            bind: "Axiom-3eck verbunden"
+        };
+    }
 };
-
-D3D11CreateDevice(
-    nullptr,
-    D3D_DRIVER_TYPE_HARDWARE,
-    nullptr,
-    0,
-    levels,
-    ARRAYSIZE(levels),
-    D3D11_SDK_VERSION,
-    &device,
-    &featureLevel,
-    &context
-);
